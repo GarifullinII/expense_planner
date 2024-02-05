3e561e8e1c3516b87044ab757d5efde5463a02dc
GarifullinII <ildaridrisovich@gmail.com>
2024-02-05 15:20:32 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# expense_planner
+
+Flutter project
---
+++ android/app/src/main/kotlin/com/volgacode/expense_planner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.volgacode.expense_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
