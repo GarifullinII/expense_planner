a7d12227628ad5b1c0a97f7d414fe9cb364a6173
GarifullinII <ildaridrisovich@gmail.com>
2024-02-12 14:24:47 +0400

    add ListView.builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,63 +12,67 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransaction.map((transaction) {
-        return Card(
-          shape: Border.all(
-            color: Colors.white,
-            width: 2,
-          ),
-          borderOnForeground: true,
-          elevation: 4,
-          color: Colors.white60,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 16,
-                  ),
-                  child: Text(
-                    '${transaction.amount} \$',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        itemCount: userTransaction.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            shape: Border.all(
+              color: Colors.white,
+              width: 2,
+            ),
+            borderOnForeground: true,
+            elevation: 4,
+            color: Colors.white60,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 16,
+                    ),
+                    child: Text(
+                      '${userTransaction[index].amount} \$',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        transaction.title,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Text(
+                          userTransaction[index].title,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      DateFormat.yMMMMd().format(transaction.date),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.black54,
+                      Text(
+                        DateFormat.yMMMMd().format(userTransaction[index].date),
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.black54,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
